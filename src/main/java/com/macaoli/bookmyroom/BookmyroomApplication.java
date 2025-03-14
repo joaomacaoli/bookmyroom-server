f2b4f02bb33c6ff4193594405a890482dbe9604b
João Macaoli <devjoaom@gmail.com>
2025-03-14 14:06:24 -0300

    feat: add basic project configuration

---
+++ src/main/java/com/macaoli/bookmyroom/BookmyroomApplication.java
@@ -0,0 +1,13 @@
+package com.macaoli.bookmyroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmyroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmyroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/macaoli/bookmyroom/BookmyroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macaoli.bookmyroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmyroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
